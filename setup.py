cfac5c0bf4024674ac89644dbbfb8f0d89e5a925
abchiyi <meme_me2019@outlook.com>
2020-11-29 00:49:24 +0800

    发布附加md信息

--- setup.py
+++ setup.py
@@ -1,27 +1,6 @@
-from setuptools import setup, find_packages
-from os import path, popen
-
-
-with open(path.join('./README.md'), encoding='utf-8') as file:
-    long_description = file.read()
-
-
+from setuptools import setup
+from os import popen
 setup(
-    name='bing_translation_for_python',
-    python_requires='>=3.6',
     # 从标准输出获取版本号
     version=popen('git tag').read().split()[-1],
-    author='87Keys',
-    author_email='meme_me2019@outlook.com',
-    long_description_content_type='text/markdown',
-    long_description=open('./README.md', encoding='UTF-8').read(),
-    packages=find_packages(),
-    classifiers=[
-        # "Programming Language:: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=[
-        i.strip()
-        for i in open('./install_requires').readlines()][:-1]
 )
